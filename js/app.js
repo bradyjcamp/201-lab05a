5392932555ba6068253404fef5c3acb0630723a1
Brady Camp <bradyjcamp@gmail.com>
2022-01-17 12:58:51 -0500

    added answer to stretch goal question

--- js/app.js
+++ js/app.js
@@ -136,10 +136,17 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+  let product = 1;
+  // cant be 0 because anything times 0 is 0
+  for(let i=0; i < dynamicArray.length; i++){
+    product = multiply(product, dynamicArray[i])[0];
+  // 2 = multiply(1, 2)[0];
+  }
+  
+  return [product, `The numbers ${dynamicArray} have a product of ${product}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
